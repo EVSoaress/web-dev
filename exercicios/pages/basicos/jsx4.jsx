2286f65cdf6ce27ae944e4b0e5d40ae98facc33f
Erick Soares <evss@cin.ufpe.br>
2022-04-23 22:54:25 -0300

    removendo arquivo js,  adicionando lista .jsx e trabalhando com integração de JS e JSX

--- exercicios/pages/basicos/jsx4.jsx
+++ exercicios/pages/basicos/jsx4.jsx
@@ -1,10 +1,12 @@
 export default function jsx4() {
     const subtitulo = "Estou no JavaScript"
+    const trechoH3 = <h3>{3 * 3}</h3>
 
     return (
         <div>
             <h1>integração JS e JSX</h1>
             <h2>{subtitulo}</h2>
+            {trechoH3}
             <h3>{entre(9.8, 1, 15)}</h3>
         </div>
     )
--- exercicios/pages/basicos/segundo.js
+++
@@ -1,8 +0,0 @@
-export default function Segundo() {
-    return (
-        <div>
-            <h1>Segundo</h1>
-            <p>Segundo alterado 2</p>
-        </div>
-    );
-}
