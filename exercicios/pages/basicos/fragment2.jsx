69e1fa687d8367650becae7ab0751c109423bd69
Erick Soares <evss@cin.ufpe.br>
2022-04-25 10:30:38 -0300

    trabalhando com fragment com import(no fragment1) e sem import, com tag vazia(no fragment2)

---
+++ exercicios/pages/basicos/fragment2.jsx
@@ -0,0 +1,11 @@
+export default function fragment(){
+    return (
+            
+            <>
+            <h1>Fragment</h1>
+            <h2>Fragment</h2>
+            </>
+            
+  
+    )
+}
